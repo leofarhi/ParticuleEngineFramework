41b88639a70a380cad8a7033a478ad760fdc2e56
Leo Farhi <leofarhi92@gmail.com>
2024-07-30 22:34:25 +0200

    ignore tests

--- Framework/testerWin.bat
+++
@@ -1,4 +0,0 @@
-python .\Framework.py build Windows "[InterfaceSystem]"
-python .\Framework.py demo Windows clean
-python .\Framework.py demo Windows Cube3D
-.\Distributions\CasioCg\Demos\Demo1\bin\main.exe
